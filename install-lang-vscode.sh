db6a6a23f4753a4cd34eeafccf10ed120c44f3fb
marco-perin <30049458+marco-perin@users.noreply.github.com>
2025-06-17 17:31:49 +0200

    Update vscode language extension

--- install-lang-vscode.sh
+++ install-lang-vscode.sh
@@ -3,6 +3,6 @@
 # Script to install the BDSL language extension for Visual Studio Code.
 # That needs to be compiled first in the subfolder. 
 
-path="editor/vscode/bdsl-lang/bdsl-lang-0.0.1.vsix"
+path="editor/vscode/bdsl-lang/bdsl-lang-0.0.2.vsix"
 code --install-extension $path
 rm $path
